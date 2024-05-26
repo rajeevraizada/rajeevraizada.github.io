52518da217828363bf2735fc38af392318d4ef71
rajeevraizada <43006710+rajeevraizada@users.noreply.github.com>
2024-05-26 11:09:13 -0400

    Add files via upload

--- Pentatonic_bouncing/script.js
+++ Pentatonic_bouncing/script.js
@@ -9,12 +9,20 @@ let bigCircleStartY = 200;
 let max_speed = 10;
 let ball_bounciness = 1.08;
 let tones_pentatonic = [0, 2, 4, 7, 9, 12];
+let initial_drop_made = 0;
 
 function draw() {
   // clear();
-  ball0.color = color(frameCount % 360, 100, 100);
-  ball1.color = color((frameCount+120) % 360, 100, 100);
-  ball2.color = color((frameCount+240) % 360, 100, 100);
+  if (initial_drop_made == 0) {
+    noStroke();
+    textSize(14);
+    text('Click anywhere to start', 120, 20);
+    stroke('black');
+  } else {
+    ball0.color = color(frameCount % 360, 100, 100);
+    ball1.color = color((frameCount + 120) % 360, 100, 100);
+    ball2.color = color((frameCount + 240) % 360, 100, 100);
+  }
 }
 
 
@@ -50,22 +58,34 @@ function setup() {
     this_ball.speed = min(this_ball.speed, 9);
     // Make the sound be one of the twelve half-tones in a scale
     direction_int = Math.round(6 * Math.abs(this_ball.direction) / 180);
-    pentatonic_ind = Math.min( Math.max(0, direction_int), 5);
+    pentatonic_ind = Math.min(Math.max(0, direction_int), 5);
     this_ball.direction_int = direction_int;
-    tone_to_play = 200 * 2**( tones_pentatonic[pentatonic_ind] / 12);
+    tone_to_play = 200 * 2 ** (tones_pentatonic[pentatonic_ind] / 12);
     synth.triggerAttackRelease(tone_to_play, 0.1);
   }
   );
 
-  ball0 = new ball_group.Sprite();
-  ball1 = new ball_group.Sprite();
-  ball2 = new ball_group.Sprite();
-  ball1.x = ball0.x + 30;
-  ball2.x = ball0.x + 60;
-  
   colorMode(HSB);
   // Make a new tone.js tone object
   synth = new Tone.PolySynth().toDestination();
 }
 
+// Play a sound on mousePress, as sound has to be user-initiated
+function mousePressed() {
+  if (initial_drop_made == 0) {
+    synth.triggerAttackRelease(200, 0.1);
+    initial_drop();
+  }
+}
 
+
+function initial_drop() {
+  if (initial_drop_made == 0) {
+    ball0 = new ball_group.Sprite();
+    ball1 = new ball_group.Sprite();
+    ball2 = new ball_group.Sprite();
+    ball1.x = ball0.x + 30 + 5 * Math.random();
+    ball2.x = ball0.x + 60 + 5 * Math.random();
+    initial_drop_made = 1;
+  }
+}
